3c08d92bd1fa50cb1b5c2cabca64c6b3000cb07c
ihabarbid <arbidihab@gmail.com>
2021-07-19 22:43:22 +0300

    commit

---
+++ 0-project_add_new/0-project_add_new.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0_project_add_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Click on the Create menu on the left side of the Project window’s toolbar");
+        }
+    }
+}
